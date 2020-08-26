564ec4b8d3a2b1947a58708176d30e2905045402
DeltaNedas <deltanedas@gmail.com>
2020-08-26 16:42:12 +0100

    2.1.0: 6.0 port

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
-var ui = require("ui-lib/library");
+const ui = require("ui-lib/library");
 
-var last = 0;
+var last = 0, lastPos = new Vec2();
 
 const speed = () => {
-	const speed = Vars.player.velocity().len2();
-	const avg = Mathf.lerp(speed, last, 1/60);
+	const pos = Tmp.v1.set(Vars.player);
+
+	const speed = pos.sub(lastPos).len();
+	const avg = Mathf.lerp(speed, last, Time.delta);
+	lastPos.set(Vars.player);
 	last = speed;
 	return "Speed: " + Math.floor(avg);
 };
 
 ui.addTable("side", "speedometer", table => {
-	table.label(prov(speed));
+	table.label(speed);
 });
