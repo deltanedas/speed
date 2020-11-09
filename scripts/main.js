fcabc275bc431ff73d133d04706dfdada3b1b444
DeltaNedas <deltanedas@gmail.com>
2020-11-09 09:05:29 +0000

    use vel

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,8 @@
 const ui = require("ui-lib/library");
 
-var last = 0, lastPos = new Vec2();
-
 const speed = () => {
-	const pos = Tmp.v1.set(Vars.player);
-
-	const speed = pos.sub(lastPos).len();
-	const avg = Mathf.lerp(speed, last, Time.delta);
-	lastPos.set(Vars.player);
-	last = speed;
-	return "Speed: " + Math.floor(avg);
+	const speed = Vars.player.unit().vel.len();
+	return "Speed: " + Math.floor(speed);
 };
 
 ui.addTable("side", "speedometer", table => {
